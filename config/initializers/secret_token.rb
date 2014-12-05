e6dc19c14954ad1b5d1c20bac08c39c51c5f5b88
John <john.romas87@gmail.com>
2014-12-05 07:20:45 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dayssince::Application.config.secret_key_base = '2389d5f5516fc5b3ecfbd1126f4815bff5fe8ba75db8cfea618f0bd323b460d6e7309faf5c0a30e99dcc3669a8f2ed428b98b84e91f1cd7b283bc60ebd0fd0b6'
